909f318ec700fd90d910b39bf65df121ac8a01de
Giorgi Sharashashenidze <gio.shara12345@gmail.com>
2020-08-21 19:28:31 +0200

    ??

---
+++ lib/pages/choose_sports_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ChooseSportsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Choose Sports!'),
+      ),
+    );
+  }
+}
